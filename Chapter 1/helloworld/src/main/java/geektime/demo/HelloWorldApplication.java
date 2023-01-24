951789e4e0023902215fae68d15cc9873a5381b4
Keeko <787018661@qq.com>
2023-01-24 22:37:43 +0800

    [feat] maven 打包项目

--- Chapter 1/helloworld/src/main/java/geektime/demo/HelloWorldApplication.java
+++ Chapter 1/helloworld/src/main/java/geektime/demo/HelloWorldApplication.java
@@ -17,5 +17,20 @@ public class HelloWorldApplication {
 	public String hello() {
 		return "Hello World!";
 	}
+
+	/*
+	打包 跳过测试
+		mvn clean package -Dmaven.test.skip
+
+	执行
+	java -jar helloworld-0.0.1-SNAPSHOT.jar
+
+		前提：
+		安装maven
+		官网下载地址:http://maven.apache.org/download.cgi
+
+		安装配置
+		https://blog.csdn.net/jasnet_u/article/details/81209942
+	*/
 }
 
